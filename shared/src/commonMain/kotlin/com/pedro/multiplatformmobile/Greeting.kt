11bf70ebb5ef2a0fde140d38139ff042fdc5bcae
pedrokorwa <korwapedro@gmail.com>
2021-09-03 13:27:12 +0700

    new

--- shared/src/commonMain/kotlin/com/pedro/multiplatformmobile/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.pedro.multiplatformmobile
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/pedro/multiplatformmobile/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.pedro.multiplatformmobile
-
-expect class Platform() {
-    val platform: String
-}
