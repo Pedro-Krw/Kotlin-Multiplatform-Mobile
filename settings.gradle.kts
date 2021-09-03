9ec9de606a661cd43c69637ca97dd0bbc8b4b4e5
pedrokorwa <korwapedro@gmail.com>
2021-09-03 13:28:10 +0700

    Kotlin Multiplatform

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Multiplatform_Mobile"
+include(":androidApp")
+include(":shared")
